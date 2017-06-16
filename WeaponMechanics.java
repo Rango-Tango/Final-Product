7004c14228a431f115c28a27ae627637cd63f67c
MadamoiselleA <anastasia.lomakina26@gmail.com>
2017-06-15 21:06:23 -0400

    Update WeaponMechanics.java

--- WeaponMechanics.java
+++ WeaponMechanics.java
@@ -3,19 +3,54 @@ public class WeaponMechanics
 {
 	static int proj=0;
 	static int bomb=0;
-	static int bombx=0;
-	static int bomby=0;
+	static int bombx=7;
+	static int bomby=7;
 	static int direction=0;
 	public static void Attack(String cw)
 	{
 		if(cw.equals("Sword"))
 		{
-			if(meanie.getX() == MovementspaceObject.getX()+1||meanie.getX() == MovementspaceObject.getX()-1||meanie.getX() == MovementspaceObject.getX())
+			switch(M_M.r.room)
 			{
-				if(meanie.getY() == MovementspaceObject.getY()+1||meanie.getY() == MovementspaceObject.getY()-1||meanie.getY() == MovementspaceObject.getY())
+			case 1:
+				if(meanie.x[1] == MovementspaceObject.getX()+1||meanie.x[1] == MovementspaceObject.getX()-1||meanie.x[1] == MovementspaceObject.getX())
 				{
-					meanie.sethealth(meanie.gethealth()-1);
+					if(meanie.y[1] == MovementspaceObject.getY()+1||meanie.y[1] == MovementspaceObject.getY()-1||meanie.y[1] == MovementspaceObject.getY())
+					{
+						meanie.hitM(cw, 1);
+					}
 				}
+				break;
+			case 2:
+				if(meanie.x[2] == MovementspaceObject.getX()+1||meanie.x[2] == MovementspaceObject.getX()-1||meanie.x[2] == MovementspaceObject.getX())
+				{
+					if(meanie.y[2] == MovementspaceObject.getY()+1||meanie.y[2] == MovementspaceObject.getY()-1||meanie.y[2] == MovementspaceObject.getY())
+					{
+						meanie.hitM(cw, 2);
+					}
+				}
+				break;
+			case 3:
+				if(meanie.x[3] == MovementspaceObject.getX()+1||meanie.x[3] == MovementspaceObject.getX()-1||meanie.x[3] == MovementspaceObject.getX())
+				{
+					if(meanie.y[3] == MovementspaceObject.getY()+1||meanie.y[3] == MovementspaceObject.getY()-1||meanie.y[3] == MovementspaceObject.getY())
+					{
+						meanie.hitM(cw, 3);
+					}
+				}
+				break;
+			case 4:
+				if(3==MovementspaceObject.getX()+1||3==MovementspaceObject.getX()-1||3==MovementspaceObject.getX())
+				{
+					if(6==MovementspaceObject.getY()+1||6==MovementspaceObject.getY()-1||6==MovementspaceObject.getY())
+					{
+						Boss.setHealth(Boss.getHealth()-1);
+						MovementspaceObject.setY(0);
+					}
+				}
+				break;
+			default:
+				break;
 			}
 
 		}
@@ -23,10 +58,17 @@ public class WeaponMechanics
 		{
 			if(proj==0)
 			{
-				proj=1;
-				Bow.Shoot(MovementspaceObject.getX(),MovementspaceObject.getY(),PlayerMovement.Getd());
+				if(M_M.r.room==4)
+				{
+					proj=1;
+					Bow.Shoot(MovementspaceObject.getX(),MovementspaceObject.getY(),PlayerMovement.Getd());
+				}
+				else
+				{
+					proj=1;
+					Bow.Shoot(MovementspaceObject.getX(),MovementspaceObject.getY(),BossListener.d);	
+				}
 			}
-			PD.Refresh();
 		}
 		else if(cw.equals("Bomb"))
 		{
@@ -40,36 +82,142 @@ public class WeaponMechanics
 			}
 			else
 			{
+				int win=0;
+				int b =0;
 				bomb--;
-				if(meanie.getX() == bombx+1||meanie.getX() == bombx-1||meanie.getX() == bombx)
+				switch(M_M.r.room)
 				{
-					if(meanie.getY() == bomby||meanie.getY() == bomby-1||meanie.getY() == bomby)
+				case 0:
+					break;
+				case 1:
+					if(meanie.x[1] == bombx+1||meanie.x[1] == bombx-1||meanie.x[1] == bombx)
 					{
-						meanie.sethealth(meanie.gethealth()-2);
+						if(meanie.y[1] == bomby||meanie.y[1] == bomby-1||meanie.y[1] == bomby)
+						{
+							meanie.sethealth(meanie.gethealth(1)-2, 1);
+						}
 					}
+					break;
+				case 2:
+					if(meanie.x[2] == bombx+1||meanie.x[2] == bombx-1||meanie.x[2] == bombx)
+					{
+						if(meanie.y[2] == bomby||meanie.y[2] == bomby-1||meanie.y[2] == bomby)
+						{
+							meanie.sethealth(meanie.gethealth(2)-2, 2);
+						}
+					}
+					break;
+				case 3:
+					if(meanie.x[3] == bombx+1||meanie.x[3] == bombx-1||meanie.x[3] == bombx)
+					{
+						if(meanie.y[3] == bomby||meanie.y[3] == bomby-1||meanie.y[3] == bomby)
+						{
+							meanie.sethealth(meanie.gethealth(3)-2, 3);
+						}
+					}
+					break;
+				case 4:
+					for(int v=bombx-1;v<bombx+2;v++)
+					{				
+						if(SB.find(v,bomby+1)!=-1||SB.find(v,bomby)!=-1||SB.find(v,bomby-1)!=-1)
+						{
+							win=1;
+							b=v;
+						}
+					}
+					if(win==1)
+					{
+						Boss.setHealth(Boss.getHealth()-1);
+						if(SB.find(b,bomby+1)!=-1)
+						{
+							SB.Hit(b,bomby+1);	
+						}
+						else if(SB.find(b,bomby)!=-1)
+						{
+							SB.Hit(b,bomby);	
+						}
+						else
+						{
+							SB.Hit(b,bomby-1);	
+						}
+					}
+					break;
+				default:
+					break;
 				}
 			}
-			PD.Refresh();
 		}
 		else if(cw.equals("Shank"))
 		{
-			if(meanie.getX() == MovementspaceObject.getX()+1||meanie.getX() == MovementspaceObject.getX()-1||meanie.getX() == MovementspaceObject.getX())
+			switch(M_M.r.room)
 			{
-				if(meanie.getY() == MovementspaceObject.getY()+1||meanie.getY() == MovementspaceObject.getY()-1||meanie.getY() == MovementspaceObject.getY())
+			case 0:
+				break;
+			case 1:
+				if(meanie.x[1] == MovementspaceObject.getX()+1||meanie.x[1] == MovementspaceObject.getX()-1||meanie.x[1] == MovementspaceObject.getX())
 				{
-					meanie.sethealth(meanie.gethealth()-2);
+					if(meanie.y[1] == MovementspaceObject.getY()+1||meanie.y[1] == MovementspaceObject.getY()-1||meanie.y[1] == MovementspaceObject.getY())
+					{
+						meanie.sethealth(meanie.gethealth(1)-2, 1);
+					}
 				}
+				break;
+			case 2:
+				if(meanie.x[2] == MovementspaceObject.getX()+1||meanie.x[2] == MovementspaceObject.getX()-1||meanie.x[2] == MovementspaceObject.getX())
+				{
+					if(meanie.y[2] == MovementspaceObject.getY()+1||meanie.y[2] == MovementspaceObject.getY()-1||meanie.y[2] == MovementspaceObject.getY())
+					{
+						meanie.sethealth(meanie.gethealth(2)-2, 2);
+					}
+				}
+				break;
+			case 3:
+				if(meanie.x[3] == MovementspaceObject.getX()+1||meanie.x[3] == MovementspaceObject.getX()-1||meanie.x[3] == MovementspaceObject.getX())
+				{
+					if(meanie.y[3] == MovementspaceObject.getY()+1||meanie.y[3] == MovementspaceObject.getY()-1||meanie.y[3] == MovementspaceObject.getY())
+					{
+						meanie.sethealth(meanie.gethealth(3)-2, 3);
+					}
+				}
+				break;
+			case 4:
+				if(3==MovementspaceObject.getX()+1||3==MovementspaceObject.getX()-1||3==MovementspaceObject.getX())
+				{
+					if(6==MovementspaceObject.getY()+1||6==MovementspaceObject.getY()-1||6==MovementspaceObject.getY())
+					{
+						Boss.setHealth(Boss.getHealth()-1);
+						MovementspaceObject.setY(0);
+					}
+				}
+				break;
+			default:
+				break;
 			}
 		}
 		else	//peanuts!
 		{
 			if(proj==0)
 			{
-				proj=2;
-				Bow.Shoot(MovementspaceObject.getX(),MovementspaceObject.getY(),PlayerMovement.Getd());
+				if(M_M.r.room==4)
+				{
+					proj=2;
+					Bow.Shoot(MovementspaceObject.getX(),MovementspaceObject.getY(),PlayerMovement.Getd());
+				}
+				else
+				{
+					proj=2;
+					Bow.Shoot(MovementspaceObject.getX(),MovementspaceObject.getY(),BossListener.d);	
+				}
 			}
 		}
-		PD.Refresh();
+		if(M_M.r.room==0 || M_M.r.room==1 || M_M.r.room==2 || M_M.r.room==3)
+		{
+			PD.Refresh();
+		}
+		else
+		{
+			BD.Refresh();
+		}
 	}
 	public static int Projectile()
 	{
@@ -78,6 +226,8 @@ public class WeaponMechanics
 	public static void Kill()
 	{
 		proj=0;
+		Bow.projectile[1]=7;
+		Bow.projectile[0]=7;
 	}
 	public static int Bombx()
 	{
